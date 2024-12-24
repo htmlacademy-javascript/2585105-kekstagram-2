387d8bc7a473dd312c3d3dad5cf5352da26c0c68
Анна Крамер <anna.pro.kramer@gmail.com>
2024-12-24 17:46:02 +0300

    добавляет функции

--- js/utils.js
+++ js/utils.js
@@ -5,4 +5,45 @@ export const createIdGenerator = () => {
   return () => ++lastId;
 };
 
+const ALERT_SHOW_TIME = 5000;
+const isEsc = (key) => key === 27;
+const isEnter = (key) => key === 13;
 
+const toggleModalElement = (modalElement) => {
+  modalElement.classList.toggle('hidden');
+  document.body.classList.toggle('modal-open');
+};
+
+
+const showAlert = (message) => {
+  const template = document.querySelector('#data-error').content.querySelector('.data-error');
+  const error = template.cloneNode(true);
+  const title = error.querySelector('.data-error__title');
+  title.textContent = message;
+  document.body.append(error);
+
+  setTimeout(() => {
+    error.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+const showImgFilterButtons = () =>
+  document.querySelector('.img-filters')
+    .classList.remove('img-filters--inactive');
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {
+  isEsc,
+  isEnter,
+  showAlert,
+  showImgFilterButtons,
+  debounce,
+  toggleModalElement,
+};
